ec9f438a532f220415b4cbb8eb1bb1bde70b77e1
Willemijn <willemijnluiten@gmail.com>
2023-12-16 23:13:00 +0100

    Made a start with part 1 of day 3

---
+++ 2023/03/solutions.py
@@ -0,0 +1,24 @@
+import re
+with open('input.txt') as f:
+    game_input = f.readlines()
+
+numeric_entries, dots, syms = [],[],[]
+for line in game_input:
+    nums = [s.isdigit() for s in line]
+    dot = [s == '.' for s in line]
+    sym = [-(a+b)+1 for a,b in zip(nums,dot)]
+    numeric_entries.append(nums)
+    dots.append(dot)
+    syms.append(sym)
+    indices = [[i for i,val in enumerate(sym_list) if val == 1] for sym_list in syms]
+
+# Find the part numbers. Part numbers are adjecent (also diagonally) to a symbol.
+# The entries where syms = 1 represent the symbols. Now we only have to find the adjecent numbers.
+# Steps to take:
+# - check the (max) 8 values around a symbol for being a number. If on left: move left until not number anymore : add all 
+    
+# other option: start from the numbers -> re.finditer('\d+',line) and use .span() to find start and end, and int(.group()) to get out the interger for summing after checking left and right around for symbols, in the symbol lattice.
+
+coordinates = []
+for y,lst in enumerate(indices):
+    coordinates+= [(y,x) for x in lst]
